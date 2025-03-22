db6c2a060acd2579ddacb3fa8c996e3d1e2f444b
fmahadyBD <fmahady01@gmail.com>
2025-03-23 04:59:31 +0600

    Add AppConstant,Controller,dao,entity,service for Java EE

---
+++ src/main/com/javaee/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.javaee.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet({"/"})
+public class IndexController extends HttpServlet {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/pages/index.jsp");
+        rd.forward(request, response);
+    }
+}
---
+++ src/main/webapp/pages/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mahady-hasan-fahim
+  Date: 3/22/25
+  Time: 9:59 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>This is cloud training !!</title>
+  </head>
+  <body>
+  
+  </body>
+</html>
